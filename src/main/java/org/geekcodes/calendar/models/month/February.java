32cde1c9b94510f079ca2b7ef4ae82bc22b131de
A Geek That Codes <5011285-robertplagge@users.noreply.gitlab.com>
2020-06-03 21:37:20 -0500

    Added February

---
+++ src/main/java/org/geekcodes/calendar/models/month/February.java
@@ -0,0 +1,58 @@
+package org.geekcodes.calendar.models.month;
+
+import org.geekcodes.calendar.utilities.DateUtility;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * The type February.
+ */
+public class February extends AMonth implements GMonth {
+    
+    /**
+     * Instantiates a new February.
+     *
+     * @param year the year
+     */
+    public February(int year) {
+        this.localDate = LocalDate.of(year, Month.FEBRUARY, 1);
+    }
+    
+    /**
+     * Gets holidays.
+     *
+     * @return the holidays
+     */
+    @Override
+    public List<LocalDate> getHolidays() {
+        List<LocalDate> holidays = new ArrayList<>();
+        
+        holidays.add(getPresidentsDay());
+        
+        return holidays;
+    }
+    
+    /**
+     * Gets presidents day.
+     *
+     * @return the presidents day
+     */
+    public LocalDate getPresidentsDay() {
+        long daysToAdd = DateUtility.getDaysUntilMonday(this.localDate);
+        return this.localDate.plusDays(daysToAdd + 14);
+    }
+    
+    /**
+     * Gets dates.
+     *
+     * @return the dates
+     */
+    @Override
+    public List<LocalDate> getDates() {
+        return this.localDate.datesUntil(LocalDate.of(this.localDate.getYear(), Month.MARCH, 1)).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/org/geekcodes/calendar/models/month/FebruaryTest.java
@@ -0,0 +1,70 @@
+package org.geekcodes.calendar.models.month;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The type February test.
+ */
+class FebruaryTest {
+    
+    /**
+     * The constant YEAR.
+     */
+    public static final int             YEAR          = 2020;
+    private             LocalDate       localDate     = LocalDate.of(2020, Month.JANUARY, 1);
+    private             List<LocalDate> februaryDates = LocalDate.of(YEAR, Month.FEBRUARY, 1).datesUntil(LocalDate.of(YEAR, Month.MARCH, 1)).collect(Collectors.toList());
+    private             February        february      = new February(YEAR);
+    
+    /**
+     * Sets up.
+     */
+    @BeforeEach
+    void setUp() {
+    }
+    
+    /**
+     * Tear down.
+     */
+    @AfterEach
+    void tearDown() {
+    }
+    
+    /**
+     * Test get holidays.
+     */
+    @Test
+    void testGetHolidays() {
+        List<LocalDate> expectedDates = new ArrayList<>();
+        expectedDates.add(LocalDate.of(YEAR, Month.FEBRUARY, 17));
+        
+        Assertions.assertEquals(expectedDates, february.getHolidays());
+    }
+    
+    /**
+     * Test get presidents day.
+     */
+    @Test
+    void testGetPresidentsDay() {
+        LocalDate expectedLocalDate = LocalDate.of(2020, Month.FEBRUARY, 17);
+        Assertions.assertEquals(expectedLocalDate, february.getPresidentsDay());
+    }
+    
+    /**
+     * Test get dates.
+     */
+    @Test
+    void testGetDates() {
+        Assertions.assertEquals(februaryDates, february.getDates());
+    }
+}
