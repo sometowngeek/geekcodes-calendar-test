15e85d19b56a39c3133d23f07a7f29e52b0bf283
A Geek That Codes <5011285-robertplagge@users.noreply.gitlab.com>
2020-06-06 09:17:28 -0500

    Added leap year check

--- src/test/java/org/geekcodes/calendar/models/month/FebruaryTest.java
+++ src/test/java/org/geekcodes/calendar/models/month/FebruaryTest.java
@@ -9,28 +9,95 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The type February test.
  */
 class FebruaryTest {
     
-    /**
-     * The constant YEAR.
-     */
-    public static final int             YEAR          = 2020;
-    private             LocalDate       localDate     = LocalDate.of(2020, Month.JANUARY, 1);
-    private             List<LocalDate> februaryDates = LocalDate.of(YEAR, Month.FEBRUARY, 1).datesUntil(LocalDate.of(YEAR, Month.MARCH, 1)).collect(Collectors.toList());
-    private             February        february      = new February(YEAR);
+    private static final int             LEAP_YEAR = 2020;
+    private              List<LocalDate> februaryDatesLeapYear;
+    private              February        februaryLeapYear;
+    
+    private static final int             NOT_LEAP_YEAR = 2021;
+    private              List<LocalDate> februaryDatesNotLeapYear;
+    private              February        februaryNotLeapYear;
     
     /**
      * Sets up.
      */
     @BeforeEach
     void setUp() {
+        februaryLeapYear    = new February(LEAP_YEAR);
+        februaryNotLeapYear = new February(NOT_LEAP_YEAR);
+        
+        initializeLeapYearDates();
+        initializeNonLeapYearDates();
+    }
+    
+    private void initializeNonLeapYearDates() {
+        februaryDatesNotLeapYear = new ArrayList<>();
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 1));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 2));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 3));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 4));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 5));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 6));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 7));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 8));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 9));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 10));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 11));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 12));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 13));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 14));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 15));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 16));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 17));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 18));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 19));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 20));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 21));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 22));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 23));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 24));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 25));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 26));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 27));
+        februaryDatesNotLeapYear.add(LocalDate.of(NOT_LEAP_YEAR, Month.FEBRUARY, 28));
+    }
+    
+    private void initializeLeapYearDates() {
+        februaryDatesLeapYear = new ArrayList<>();
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 1));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 2));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 3));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 4));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 5));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 6));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 7));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 8));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 9));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 10));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 11));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 12));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 13));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 14));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 15));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 16));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 17));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 18));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 19));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 20));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 21));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 22));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 23));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 24));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 25));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 26));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 27));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 28));
+        februaryDatesLeapYear.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 29));
     }
     
     /**
@@ -46,17 +113,17 @@ class FebruaryTest {
     @Test
     void testGetHolidays() {
         List<LocalDate> expectedDates = new ArrayList<>();
-        expectedDates.add(LocalDate.of(YEAR, Month.FEBRUARY, 17));
+        expectedDates.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 17));
         
-        Assertions.assertEquals(expectedDates, february.getHolidays());
+        Assertions.assertEquals(expectedDates, februaryLeapYear.getHolidays());
     }
     
     @Test
-    void testGetHolidaysStatic(){
+    void testGetHolidaysStatic() {
         List<LocalDate> expectedDates = new ArrayList<>();
-        expectedDates.add(LocalDate.of(YEAR, Month.FEBRUARY, 17));
-    
-        Assertions.assertEquals(expectedDates, February.getHolidays(YEAR));
+        expectedDates.add(LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 17));
+        
+        Assertions.assertEquals(expectedDates, February.getHolidays(LEAP_YEAR));
     }
     
     
@@ -65,15 +132,25 @@ class FebruaryTest {
      */
     @Test
     void testGetPresidentsDay() {
-        LocalDate expectedLocalDate = LocalDate.of(2020, Month.FEBRUARY, 17);
-        Assertions.assertEquals(expectedLocalDate, february.getPresidentsDay());
+        LocalDate expectedLocalDate = LocalDate.of(LEAP_YEAR, Month.FEBRUARY, 17);
+        Assertions.assertEquals(expectedLocalDate, februaryLeapYear.getPresidentsDay());
     }
     
     /**
      * Test get dates.
      */
     @Test
-    void testGetDates() {
-        Assertions.assertEquals(februaryDates, february.getDates());
+    void testGetDatesLeapYear() {
+        Assertions.assertEquals(februaryDatesLeapYear, februaryLeapYear.getDates());
+        Assertions.assertNotEquals(februaryDatesNotLeapYear, februaryLeapYear.getDates());
+    }
+    
+    /**
+     * Test get dates.
+     */
+    @Test
+    void testGetDatesNotLeapYear() {
+        Assertions.assertEquals(februaryDatesNotLeapYear, februaryNotLeapYear.getDates());
+        Assertions.assertNotEquals(februaryDatesLeapYear, februaryNotLeapYear.getDates());
     }
 }
